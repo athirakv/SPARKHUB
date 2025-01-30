313effee8a0647fc1c5790544d3952ea28d24245
ATLASCOPCO\a00557094 <athira.valsakumar@atlascopco.com>
2025-01-30 17:49:59 +0530

    JS commit

--- force-app/main/default/lwc/availableConfigs/availableConfigs.js
+++ force-app/main/default/lwc/availableConfigs/availableConfigs.js
@@ -1,3 +1,59 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import getAvailableConfigs from '@salesforce/apex/ConfigController.getAvailableConfigs';
+import addConfigsToCase from '@salesforce/apex/ConfigController.addConfigsToCase';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class AvailableConfigs extends LightningElement {}
+export default class AvailableConfigs extends LightningElement {
+    @api recordId; // Case ID
+    configs = [];
+    selectedConfigIds = [];
+
+    @wire(getAvailableConfigs)
+    wiredConfigs({ error, data }) {
+        if (data) {
+            this.configs = data.map(config => ({ ...config, selected: false }));
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    handleRowSelection(event) {
+        const selectedRows = event.detail.selectedRows;
+        this.selectedConfigIds = selectedRows.map(row => row.Id);
+    }
+
+    handleAddConfigs() {
+        if (this.selectedConfigIds.length === 0) {
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: 'Error',
+                    message: 'No configs selected.',
+                    variant: 'error'
+                })
+            );
+            return;
+        }
+
+        addConfigsToCase({ caseId: this.recordId, configIds: this.selectedConfigIds })
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Configs added successfully.',
+                        variant: 'success'
+                    })
+                );
+                this.selectedConfigIds = [];
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+                console.error(error);
+            });
+    }
+}
--- force-app/main/default/lwc/caseConfigs/caseConfigs.js
+++ force-app/main/default/lwc/caseConfigs/caseConfigs.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import getCaseConfigs from '@salesforce/apex/ConfigController.getCaseConfigs';
+import sendCaseData from '@salesforce/apex/ConfigController.sendCaseData';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
-export default class CaseConfigs extends LightningElement {}
+export default class CaseConfigs extends LightningElement {
+    @api recordId; // Case ID
+    caseConfigs = [];
+    isSendDisabled = false;
+
+    @wire(getCaseConfigs, { caseId: '$recordId' })
+    wiredCaseConfigs({ error, data }) {
+        if (data) {
+            this.caseConfigs = data;
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    handleSend() {
+        this.isSendDisabled = true;
+        sendCaseData({ caseId: this.recordId })
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Data sent successfully. Case is now closed.',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch(error => {
+                this.isSendDisabled = false;
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error',
+                        message: error.body.message,
+                        variant: 'error'
+                    })
+                );
+                console.error(error);
+            });
+    }
+}
